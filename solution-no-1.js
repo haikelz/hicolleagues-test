1addd3861f08db09497228085a084075379eb327
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-08-15 06:22:34 +0700

    feat: txt version

--- solution-no-1.js
+++ solution-no-1.js
@@ -1,21 +1,28 @@
-function solution(num) {
+function solution1(num) {
+  // variable string yang akan direturn
   let tempStr = ``;
 
+  // loop secara menurun dari num sampai 1
   for (let i = num; i >= 1; i--) {
+    // inisialisasi variable string penampung per baris
     let newStr = ``;
 
+    // loop secara naik dari 1 sampai nilai i
     for (let j = 1; j <= i; j++) {
       if (i % 2 === 0) {
+        // masukkan string "+" jika i adalah bilangan genap
         newStr += "+";
       } else {
+        // jika tidak, maka masukkan value dari j
         newStr += j;
       }
     }
 
+    // format string per baris
     tempStr += `${newStr}\n`;
   }
 
   return tempStr;
 }
 
-console.log(solution(5));
+console.log(solution1(5));
